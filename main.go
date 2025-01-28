1b902f7514d8cd53abc7ab1d43f2df2244fed736
Valeria Fedoruk <valeria.fed@bk.ru>
2025-01-28 18:59:50 +0300

    added method for getting chess board string

--- main.go
+++ main.go
@@ -4,6 +4,41 @@ import (
 	"fmt"
 )
 
+func GetChessBoard(size int) string {
+	if size <= 0 {
+		fmt.Println("Size cannot be less or equal 0")
+	}
+
+	white := " "
+	black := "#"
+	result := ""
+	emptyMatrix := make([][]string, size)
+
+	for i := range emptyMatrix {
+		emptyMatrix[i] = make([]string, size)
+	}
+
+	for i := range size {
+		for j := range size {
+			if (i+j)%2 == 0 {
+				emptyMatrix[i][j] = white
+			} else {
+				emptyMatrix[i][j] = black
+			}
+
+			result += emptyMatrix[i][j]
+		}
+
+		result += "\n"
+	}
+
+	return result
+
+}
+
 func main() {
-	fmt.Println("First homework is here!")
+
+	board := GetChessBoard(8)
+	fmt.Print(board)
+
 }
