f62459e5b3c7b3514e601c5e95ee238c29fdce53
Valeria Fedoruk <valeria.fed@bk.ru>
2025-03-17 19:43:08 +0400

    fixed User struct

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main.go
+module otus
 
 go 1.23.4
--- internal/model/user/user.go
+++ internal/model/user/user.go
@@ -1,17 +1,33 @@
 package user
 
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"errors"
+)
+
+type Role int
+
+const (
+	Creator Role = iota
+	Manager 
+)
+
 type User struct {
-	id   int
-	Name string
-	role string
+	id       int
+	Name     string
+	role     Role
+	password string
 }
 
-func (user *User) GetRole() string {
-	return user.role
-}
-
-func (user *User) SetRole(role string) string {
+func (user *User) SetRole(role Role) {
 	user.role = role
+}
+
+func NewUser(name string, role Role, id int) User {
+	return User{Name: name, role: role, id: id}
+}
+func (user *User) GetRole() Role {
 	return user.role
 }
 
@@ -22,4 +38,25 @@ func (user *User) GetId() int {
 func (user *User) SetId(id int) int {
 	user.id = id
 	return user.id
+}
+
+func (user *User) SetPassword(password string) {
+	passByBytes := []byte(password)
+	hasher := sha1.New()
+	hasher.Write(passByBytes)
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	user.password = sha
+}
+
+func (user *User) ComaprePassword(password string) (string, error) {
+	passByBytes := []byte(password)
+	hasher := sha1.New()
+	hasher.Write(passByBytes)
+	encodedPass := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+
+	if encodedPass == user.password {
+		return "password is correct", nil
+	} else {
+		return "", errors.New("password is wrong")
+	}
 }
